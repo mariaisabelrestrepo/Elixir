31b20557644b3d8cba9351a70495170669c86f29
Maria Isabel Restrepo <maragude@bancolombia.com.co>
2023-08-18 22:21:29 -0500

    Ajuste ambos juegos

--- pares_y_nones/lib/pares_y_nones_2.ex
+++ pares_y_nones/lib/pares_y_nones_2.ex
@@ -13,13 +13,13 @@ IO.puts("2......")
 IO.puts("3......")
 :timer.sleep(2000)
 
-usuario = IO.gets("¿Cuál es tu número?(1 a 10) ") |> String.trim() |> String.to_integer()
-computadora = Enum.random(1..10)
+usuario = IO.gets("¿Cuál es tu número?(1 a 5) ") |> String.trim() |> String.to_integer()
+computadora = Enum.random(1..5)
 
 IO.puts("Mi número es #{computadora}")
 suma = computadora + usuario
 
-if usuario >= 1 and usuario <= 10 and (eleccion == "pares" or eleccion == "nones") do
+if usuario >= 1 and usuario <= 5 and (eleccion == "pares" or eleccion == "nones") do
   if (rem(suma, 2) == 0 and eleccion == "pares") or (rem(suma, 2) != 0 and eleccion == "nones") do
     IO.puts("¡Ganaste! La suma es #{suma}")
   else
@@ -27,6 +27,6 @@ if usuario >= 1 and usuario <= 10 and (eleccion == "pares" or eleccion == "nones
   end
 else
   IO.puts("Ha ingresado un valor no válido.")
-  IO.puts("Recuerda que solo tenemos 10 dedos en nuestras manos =D")
+  IO.puts("Recuerda que solo tenemos 5 dedos en cada mano =D")
   IO.puts("Y solo puedes elegir entre pares y nones. ¡Inténtalo de nuevo!")
 end
